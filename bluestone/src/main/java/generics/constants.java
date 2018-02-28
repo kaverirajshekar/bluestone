8040ab0c988d20675beccd733c03a37332ea5f70
dashreeanil <dashreeanil@gmail.com>
2018-02-28 12:35:05 +0530

    first commit

---
+++ bluestone/src/main/java/generics/constants.java
@@ -0,0 +1,10 @@
+package generics;
+
+public interface constants {
+
+	
+	String url = "https://www.bluestone.com/";
+	String userName = "admin";	
+    String password = "manager";			
+    String browser = "chrome";
+}
